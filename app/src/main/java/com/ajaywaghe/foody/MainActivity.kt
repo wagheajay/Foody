78c3bd26d9415a1f9647725183be9c6179e2b3a9
Ajay Waghe <waghe.ajay155@gmail.com>
2022-02-16 21:05:54 +0530

    second commit
    ->created navGraph
    ->created Fragments
    ->created Bottom navigationView
    ->added controller code in activity and bottom navigationview fragments clicks
    
    ->created recipes row layout for recyclerView

--- app/src/main/java/com/ajaywaghe/foody/MainActivity.kt
+++ app/src/main/java/com/ajaywaghe/foody/MainActivity.kt
@@ -2,10 +2,43 @@ package com.ajaywaghe.foody
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+   private lateinit var  navController : NavController
+   //private lateinit var bottomNavigationView : BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.navHostFragment) as NavHostFragment
+
+        //navController = findNavController(R.id.navHostFragment) as NavController
+        navController = navHostFragment.navController
+
+        val appBarConfiguration:AppBarConfiguration = AppBarConfiguration(setOf(
+            R.id.recipesFragment,
+            R.id.favoriteRecipesFragment,
+            R.id.foodJokeFragment
+        ))
+
+        bottomNavigationView.setupWithNavController(navController)
+
+        setupActionBarWithNavController(navController,appBarConfiguration)
+    }
+
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
